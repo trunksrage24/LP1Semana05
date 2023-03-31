28ab44e572ceb55555351af7faf739dd70cc91bd
Vitor <vitor.daniel@outlook.pt>
2023-03-31 10:31:56 +0100

    started ex4

---
+++ Tempo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
