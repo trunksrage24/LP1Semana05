c0cf202a4a517b5b729143a2b013463de4b0d537
Vitor <vitor.daniel@outlook.pt>
2023-03-31 11:36:20 +0100

    started ex7

---
+++ RandomDice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create dices
+            Console.WriteLine("insert number of dices: ");
+            int DiceNumber = int.Parse(Console.ReadLine());
+
+            //random roll dice
+            Random random = new Random();
+            for (int i = 0; i < DiceNumber; i++)
+            {
+                int Roll = random.Next(1,7);
+                //print dices and result
+                Console.WriteLine("rolled " + (i+1) + ":" + Roll);
+            }
+        }
+    }
+}
