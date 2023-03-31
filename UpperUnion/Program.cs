4327d7d64e8d45fd6ba02c7c4c49e6719263404e
Vitor <vitor.daniel@outlook.pt>
2023-03-31 11:56:28 +0100

    started ex 9

---
+++ UpperUnion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UpperUnion
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("format string to print in caps; sort alphabetically each array; unite strings with '-' and print");
+        }
+    }
+}
