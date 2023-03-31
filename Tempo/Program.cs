fde061fd3a18717bc4529bbeae8fc3110600aac8
Vitor <vitor.daniel@outlook.pt>
2023-03-31 10:46:11 +0100

    created stopwatches, paused them and show time elapsed

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Tempo
 {
@@ -6,7 +7,40 @@ namespace Tempo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //create 2 stopwatches
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+            
+            //start crono1 timer
+            crono1.Start();
+            
+            //pause for 500 ms crono1
+            Thread.Sleep(500);
+            
+            //start crono 2 timer
+            crono2.Start();
+            
+            //pause for 300ms crono2
+            Thread.sleep(300);
+            
+            //stop both stopwatches
+            Stopwatch.Stop();
+            
+            //show time for each
+            ElapsedTime1 = crono1.Elapsed;
+            ElapsedTime2 = crono2.Elapsed;
+            
+            //show timer in console crono1
+            string timeElapsed1 = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+                ElapsedTime1.Hours, ElapsedTime1.Minutes, ElapsedTime1.Seconds, ElapsedTime1.Milliseconds / 10);
+            
+            //show timer in console crono2
+            string timeElapsed2 = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+                ElapsedTime2.Hours, ElapsedTime2.Minutes, ElapsedTime2.Seconds, ElapsedTime2.Milliseconds / 10);
+            
+            //show in console
+            Console.WriteLine("Runtime crono1 " + timeElapsed1);
+            Console.WriteLine("Runtime crono1 " + timeElapsed2);
         }
     }
 }
